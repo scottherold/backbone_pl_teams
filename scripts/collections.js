23b899cc6108e04a7aea687caf6e8054a0d5854b
Scott Herold <41196046+scottherold@users.noreply.github.com>
2019-03-11 10:22:09 -0400

    Commit 3
    Placed JS files in proper folder.

--- scripts/collections.js
+++ scripts/collections.js
@@ -4,6 +4,3 @@
 var TeamList = Backbone.Collection.extend({
     model: Team
 });
-
-// Creates the teamList variable with the list of teams in the models.js file
-var teamList = new TeamList([team1, team2, team3, team4, team5]);
--- scripts/views.js
+++ scripts/views.js
@@ -4,78 +4,76 @@
 var TeamView = Backbone.View.extend({
     // DOM tag
     tagName: 'li',
+    className: 'team',
 
     // template -- On DOM, using jQuery refenence
     template: _.template($('#teamTemplate').html()),
-    intitialize: function(){
-        this.render();
-    },
 
     // Passes model data to the template and then to the DOM tag listed in tagName
     render: function(){
         this.$el.html(this.template(this.model.toJSON()));
         return this;
     },
-
+    
+    // Event loaded on team 'click'
     events: {
-        'click': 'viewHistory'
+        'click': 'showTeamInfo'
     },
 
-    // function to populate the view history div
-    viewHistory: function(event){
-        // Clears the element
-        $('#team_history').empty();
+    // showTeamInfo function for click event
+    showTeamInfo: function(){
 
-        // creates variables for the element
-        var teamName = event.currentTarget.id;
-        var team = myTeamList.where({name: teamName})[0];
-
-        var teamHistory = new TeamHistory(team);
-
-        $('#team_history').append(teamHistory.render());
-        return this;
-    }
-});
-
-// View for the history Model
-var TeamHistory = Backbone.View.extend({
-    // DOM tag
-    tagName: 'li',
-
-    // template -- On DOM, using jQuery refenence
-    template: _.template($('#historyTemplate').html()),
-    intitialize: function(){
-        this.render();
-    },
-
-    // Passes model data to the template and then to the DOM tag listed in tagName
-    render: function(){
-        this.$el.html(this.template(this.model.toJSON()));
-        return this;
+        // Creates a new TeamHistoryView and renders it
+        var teamHistory = new TeamHistoryView({model: this.model});
+        teamHistory.render();
     }
 });
 
 // View for the Collection Model
 var TeamListView = Backbone.View.extend({
+    
     // assigns the 'scoped' DOM element
     el: '#team_names',
     initialize: function(){
-        this.render()
+        // Adds a listener for 'add'
+        this.listenTo(this.collection, 'add', this.render);
     },
+
     render: function(){
         // failsafe to empty out the 'el' list above
-        this.$el.empty();
+        this.$el.empty()
 
-        // Takes a collection as an argument and iterates through each member, setting each Model into a View for the Model
+        // Create the collection using the 'team' class set in TeamView
         this.collection.each(function(team){
-            var teamView = new TeamView({model: team})
-            
-            // accesses the $el from the TeamView object and renders it within the TeamListView el listed above
+            var teamView = new TeamView({model: team});
             this.$el.append(teamView.render().$el);
-
-            // final this to provide context for the .each() method above
         }, this);
+
+        return this;
     }
 });
 
-var myTeamList = new TeamListView({collection: teamList});
+// View for the Team History Model
+var TeamHistoryView = Backbone.View.extend({
+
+    // assigns the 'scoped' DOM element
+    el: '#output',
+
+    // template -- On DOM, using jQuery refenence
+    template: _.template($('#historyTemplate').html()),
+    intitialize: function(){
+
+        // Listens to the View being added -- this is done within the TeamView with the 'showTeamInfo' function
+        this.listenTo(this.model, 'add', this.render);
+    },
+
+    // Passes model data to the template and then to the DOM tag listed in tagName
+    render: function(){
+        this.$el.html(this.template(this.model.toJSON()));
+    },
+});
+
+// Creates the teamList variable with the list of teams in the models.js file
+var teams = new TeamList()
+var teamList = new TeamListView({collection: teams});
+teams.add([team1, team2, team3, team4, team5]);
